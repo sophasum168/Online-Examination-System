efa1c870ff05eacf0ffc99dcfbcc62e37e09ebdf
Sokunthaneth Chhoy <chhoysokunthaneth@gmail.com>
2018-06-27 21:01:21 +0700

    add, add/new, delete
    Add button improve functionality

--- test_management/views.py
+++ test_management/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import json
+from datetime import datetime
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Test
@@ -26,21 +27,18 @@ def import_question(request):
 
 def add_test(request):
     if request.method == 'POST':
-        test_name = request.POST.get('test_name')
-        test_type = request.POST.get('test_type')
-        test_date = request.POST.get('test_date')
+        test_name = json.loads(request.POST.get('test_name'))
+        test_type = json.loads(request.POST.get('test_type'))
+        test_date = json.loads(request.POST.get('test_date'))
+        test_date = datetime.strptime(test_date, '%Y-%m-%d').date()
         test_obj = Test(test_name = test_name, test_type = test_type, test_date = test_date)
         test_obj.save()
         return HttpResponseRedirect('/test-management/test/')
-
-        #tests = Test.objects.all()
-    # return render(request, 'test_list.html')
 
 def delete_test(request):
     if request.is_ajax():
         selected_tests = request.POST['test_list_ids']
         selected_tests = json.loads(selected_tests)
-        print request.method
         for i, test in enumerate(selected_tests):
             Test.objects.filter(id__in=selected_tests).delete()
         return HttpResponseRedirect('/test-management/test/')
