3514c0738c043b46c8982109187c4ae23e6de766
sokmengpsm <34527171+sokmengpsm@users.noreply.github.com>
2018-12-03 14:07:44 +0700

    invalid
    emailin

--- register/forms.py
+++ register/forms.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django import forms
 from django.forms import ModelForm
 from .models import *
+# sokmeng
+# from verified_email_field.forms import VerifiedEmailField
 # from .models import CandiateImage
 
 # class RegisterForm(forms.ModelForm):
@@ -24,24 +26,29 @@ class UploadFileForm(forms.ModelForm):
    class Meta:
    	model = Image
    	fields = ['file']
+# sokmeng
+class UserForm(forms.ModelForm):
+   class Meta:
+        model = User
+        fields = ('email',)
+   def clean_email(self):
+        # Get the email
+        email = self.cleaned_data['email']
+        # email = self.cleaned_data.get('email')
 
-# class UserForm(forms.ModelForm):
-#    class Meta:
-#         model = User
-#         fields = ('email',)
-#    def clean_email(self):
-#         # Get the email
-#         email = self.cleaned_data.get('email')
+        # Check to see if any users already exist with this email as a username.
+        try:
+        	mt = verified_email(email)
+            # match = User.objects.get(email=email)
+        except:
+        	return forms.ValidationError("Email is not in correct format")
+        # except User.DoesNotExist:
+            # Unable to find a user, this is fine
+            # return email
 
-#         # Check to see if any users already exist with this email as a username.
-#         try:
-#             match = User.objects.get(email=email)
-#         except User.DoesNotExist:
-#             # Unable to find a user, this is fine
-#             return email
-
-#         # A user was found with this as a username, raise an error.
-#         raise forms.ValidationError('This email address is already in use.')
+        # A user was found with this as a username, raise an error.
+        return email
+        # raise forms.ValidationError('This email address is already in use.')
 
 # class FileUpload(forms.ModelForm):
 # 	class Meta:
@@ -53,3 +60,20 @@ class VideoUploadForm(forms.ModelForm):
         model = VideoUpload
         fields = ('file',)
         widgets = {'file': forms.HiddenInput()}
+
+
+#sokmeng
+
+ # def sendEmail(self, datas):
+ # 	link="http://yourdomain.com/activate/"+datas['activation_key']
+ #    c=Context({'activation_link':link,'username':datas['username']})
+ #    f = open(MEDIA_ROOT+datas['email_path'], 'r')
+ #    t = Template(f.read())
+ #    f.close()
+ #    message=t.render(c)
+ #        #print unicode(message).encode('utf8')
+ #    send_mail(datas['email_subject'], message, 'yourdomain <no-reply@yourdomain.com>', [datas['email']], fail_silently=False)
+
+ #sokmeng
+ # class RegistrationForm(forms.ModelForm):
+ # 	email = VerifiedEmailField(label='email', required=True)
