f27d97b2b2f284b9b706af77ae683bf33e97272c
Sopha <sophasum02@gmail.com>
2018-12-07 14:36:13 +0700

    removeland

--- Landingpage/views.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-
-# Create your views here.
-def landingpage(request):
-    return render(request, 'landingpage.html')
